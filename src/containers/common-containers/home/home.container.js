6bd23c4ac88bcfb538c14ddbee54f2e1cae57b32
anatol <anatoool@yandex.ru>
2019-04-09 22:38:25 +0700

    set up environment

--- src/containers/common-containers/home/home.container.js
+++ src/containers/common-containers/home/home.container.js
@@ -10,7 +10,7 @@ export class HomeContainer extends React.Component {
   render() {
     return (
       <div className="home-container">
-        Home
+        Home container
       </div>
     );
   }
